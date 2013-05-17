8f99f2bb96896ec66fa93cef729474c06dd6f222
konaka-h <konaka-h@h-konaka.local>
2013-05-17 13:48:25 +0900

    Initial Commit

---
+++ SampleStudy/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SampleStudy
+//
+//  Created by konaka-h on 2013/05/17.
+//  Copyright (c) 2013年 konaka-h. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SampleStudy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleStudy
+//
+//  Created by konaka-h on 2013/05/17.
+//  Copyright (c) 2013年 konaka-h. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
